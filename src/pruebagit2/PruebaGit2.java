0cba18c0a3af6c8dfac7ee0530b3e327c4d6e638
Jorge Martín Cabello <jmartin@iesruizgijon.com>
2023-05-19 14:00:03 +0200

    Añadida clase1 y sout HOLA MUNDO en principal

--- src/pruebagit2/PruebaGit2.java
+++ src/pruebagit2/PruebaGit2.java
@@ -16,6 +16,9 @@ public class PruebaGit2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("HOLA MUNDO");
     }
-    
+     
+         
 }
